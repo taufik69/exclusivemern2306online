5181c19ca6603291bd86a926b96cfcb3c9eb34bc
taufik69 <taufik35-2915@diu.edu.bd>
2024-11-04 02:08:23 +0600

    exclusive

---
+++ src/pages/Home/Index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/RootLayout/Header/Index";
+import Navbar from "../../components/RootLayout/Navbar/Index";
+import Banner from "../../components/homePage/Banner/Index";
+const Homepage = () => {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <Banner />
+    </div>
+  );
+};
+
+export default Homepage;
