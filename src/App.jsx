c8b0369135825463ef3695a93ff45322ae18ab5b
taufik69 <taufik35-2915@diu.edu.bd>
2024-12-23 19:41:52 +0600

    add contact page and validation schema

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import WishList from "./components/WishListComponent/Index";
 import AddToCart from "./pages/AddtoCart/AddToCart";
 import MYAcount from "./pages/myAcount/Index";
 import About from "./pages/About/Index";
+import Contact from "./pages/Contact/Contact";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
@@ -28,6 +29,7 @@ const router = createBrowserRouter(
         <Route path="/addtocart" element={<AddToCart />}></Route>
         <Route path="/myacount" element={<MYAcount />}></Route>
         <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
       </Route>
     </Route>
   )
--- src/Validation/Schema/LoginSchema.jsx
+++ src/Validation/Schema/LoginSchema.jsx
@@ -2,6 +2,7 @@ import * as Yup from "yup";
 let mailFormat = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/;
 let passwordFormat =
   /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/;
+const bdPhoneNumberRegex = /^(?:\+?88)?01[3-9]\d{8}$/;
 
 export const loginSchema = Yup.object({
   emailorphone: Yup.string()
@@ -16,4 +17,15 @@ export const loginSchema = Yup.object({
       "password inclues 1 spceacila character number Uppercase and lowecase"
     )
     .required("Password Required"),
+});
+
+export const contactSchema = Yup.object({
+  name: Yup.string().required("Name is Required"),
+  email: Yup.string()
+    .matches(mailFormat, "Invalid email address")
+    .required("Email is Required"),
+  phone: Yup.string()
+    .required("Phone Number is Required")
+    .matches(bdPhoneNumberRegex, "Invalid Phone Number"),
+  message: Yup.string().required("Message is Required"),
 });
